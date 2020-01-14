739d5c5953bec1378ee3130a2c1816e0288ceae7
yannpeso <tukusi.iwatobipennginn@gmail.com>
2020-01-15 00:07:47 +0900

    Add users to front

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
+# ルーティングを追加
 Rails.application.routes.draw do
-  get 'messages/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
+  devise_for :users
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end
