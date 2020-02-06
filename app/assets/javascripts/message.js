5f54d107f9066afdfd3f3df9921605a1aa4a374b
yannpeso <tukusi.iwatobipennginn@gmail.com>
2020-02-06 16:56:44 +0900

    Update Ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,8 +54,7 @@ $('#new_message').on('submit', function(e){
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);      
-       $('form')[0].reset();
-       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-  })
+    $('form')[0].reset();})
+    $('.box').animate({'height' : '200px'});
 })
 });
